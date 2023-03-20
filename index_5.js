1d075fd74c15eba9c97409dc832a61e85ba73cf6
git-pages <vlada.prudnikova.2004@gmail.com>
2023-03-20 12:50:29 -0400

    Initial commit

---
+++ index_5.js
@@ -0,0 +1,7 @@
+let x = prompt("введіть число х");
+let y = prompt("введіть число y");
+let z = prompt("введіть число z");
+x = parseFloat(x);
+y = parseFloat(y);
+z = parseFloat(z);
+alert("середнє арифметичне = " + (x + y + z) / 3);
